458e487d6aa408ae19cd819912c6f72325a5ca19
nikartik <110419961+NikartikTheBear@users.noreply.github.com>
2023-04-29 11:36:27 +0200

    feat/localization/it.sql) created it sql

---
+++ localization/it_esx_dmvschool.sql
@@ -0,0 +1,6 @@
+INSERT INTO `licenses` (`type`, `label`) VALUES
+	('dmv', 'Foglio Rosa'),
+	('drive', 'Patente Auto (B)'),
+	('drive_bike', 'Patente Moto (A)'),
+	('drive_truck', 'Patente Camion (C)')
+;
